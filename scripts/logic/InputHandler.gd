2a86afed4a1ec2201f127e56d32582ca3b786a92
furan0 <augustin.delecroix@gmail.com>
2023-09-30 12:49:05 +0200

    Player state machine added. Tackle/charge input handled

--- scripts/logic/InputHandler.gd
+++ scripts/logic/InputHandler.gd
@@ -6,27 +6,62 @@ class_name InputHandler
 ## Those inputs can also be spoofed by calling appropriate functions
 
 # === Inputs Signals
-## Emitted when a charge/tackle input is changed
+## Emitted when a charge input is changed
 signal chargeStatusUpdated(status : bool)
+## Emitted when a tackle is requested
+signal tackleRequested()
 ## Emitted when a guard input is changed
 signal guardStatusUpdated(status : bool)
 ## Emitted when a move is requested
 signal moveRequested(dir : Vector2)
 
 
+# ==== tackle detection mechanism
+## delay between a tackle input & a charge start signal, in s
+@export var chargeDelay : float = 0.2
+# count the number of s since tchargeDelayhe beginning of the tackle/charge press
+var ellapsedTackleTime := 0.0
+# set to true when a charge is ongoing
+var chargeOnGoing : bool = false
+
+## verbose log
+@export var isVerbose : bool = false
+
 ## Check all inputs to send appropriate signals
-func _physics_process(_delta):
-	## check charge
+func _physics_process(delta):
+	## check charge & tackle
+	if chargeOnGoing:
+		# a charge is ongoing, only check for release t osend charge stop
+		if Input.is_action_just_released("charge"):
+			_print("Charge ended")
+			chargeStatusUpdated.emit(false)
+			chargeOnGoing = false
+			ellapsedTackleTime = 0.0
+	elif Input.is_action_pressed("charge"):
+		#input pressed but not yet in charge
+		ellapsedTackleTime += delta
+		if (ellapsedTackleTime >= chargeDelay):
+			# it is not a tackle anymore, but the start of a charge !
+			chargeOnGoing = true
+			chargeStatusUpdated.emit(true)
+			_print("Charge started")
+	elif Input.is_action_just_released("charge"):
+		# Charge button released before entering charge -> tackle
+		tackleRequested.emit()
+		ellapsedTackleTime = 0.0
+		_print("Tackle requested")
+			
 	if Input.is_action_just_pressed("charge"):
 		chargeStatusUpdated.emit(true)
-	if Input.is_action_just_released("charge"):
-		chargeStatusUpdated.emit(false)
+	
 	
 	## check guard
 	if Input.is_action_just_pressed("guard"):
 		guardStatusUpdated.emit(true)
+		_print("Guard started")
 	if Input.is_action_just_released("guard"):
 		guardStatusUpdated.emit(false)
+		_print("Guard ended")
 	
 	
 	## check move
@@ -44,3 +79,7 @@ func spoofInput(inputName : String, value):
 			guardStatusUpdated.emit(value)
 		"move":
 			moveRequested.emit(value)
+
+func _print(text : String):
+	if isVerbose:
+		print(text)
