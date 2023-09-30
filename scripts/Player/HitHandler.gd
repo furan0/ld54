ba126b2ba0452f590dad382ff186ae164b0aa974
furan0 <augustin.delecroix@gmail.com>
2023-09-30 16:59:00 +0200

    Tackle added

---
+++ scripts/Player/HitHandler.gd
@@ -0,0 +1,55 @@
+extends Node
+class_name HitHandler
+## handle player hit and protection
+##
+## This script receive stuns, impulse & hits
+
+## Set to true to make someone invulnerable
+@export var invulnerable : bool = false
+
+## rigidbody to influence. if not set, will take its parent
+@export var rigidbody : RigidBody2D
+
+##verbose mode
+@export var isVerbose : bool = false
+
+## Check if we are protected or not
+var isProtected : bool = false
+
+
+## stun signal
+signal startStun(duration : float)
+
+## Call this function to hit the player
+## Params :
+## - stunDuration : duration of the stun associated with the hit
+## - impactVector : impact Vector for hit impulse calculation
+## - bypassProtection : can this hit bypass guard protection
+func hit(stunDuration : float, impactVector : Vector2 = Vector2.ZERO, bypassProtection : bool = false):
+	# Check if we can receive the hit
+	if invulnerable:
+		_print("Received a hit while invulnerable. Ignoring it")
+		return
+	
+	if isProtected && !bypassProtection:
+		_print("Received a non-bypassing hit while protected. Ignoring it")
+		return
+	
+	# Not protected -> receive the hit
+	_print("Character hit and stuned for " + str(stunDuration))
+	startStun.emit(stunDuration)
+	rigidbody.apply_impulse(impactVector)
+	rigidbody.rotate(impactVector.angle() + PI) # make the player face the blow
+
+
+func _ready():
+	# if rigidbody not set, take its parent if compatible
+	if (rigidbody == null):
+		if is_instance_of(get_parent(), RigidBody2D):
+			rigidbody = get_parent() as RigidBody2D
+		else:
+			push_error("Rigidbody must be set or the node parent must be of type RigidBody2D")
+
+func _print(text : String):
+	if isVerbose:
+		print(text)
---
+++ scripts/Player/TackleHandler.gd
@@ -0,0 +1,64 @@
+@tool
+extends Node
+class_name TackleHandler
+## Handle tackles
+##
+## This script handle tackles and stun enemy if in the area
+
+## Area2D used for this tackler
+@export var hitbox : Area2D
+
+## Ennemy stun duration after receiving this tackle
+@export var stunDuration : float = 1.0
+
+## Is verbose flag
+@export var isVerbose : bool = false
+
+
+## Bodies currently in the hitbox
+var hitablesBody : Array[Node2D] = []
+
+func _ready():
+	if hitbox == null:
+		push_warning("Hitbox not set. doing nothing")
+		return
+	
+	# Connect hitbox signals to body list
+	hitbox.body_entered.connect(func (node : Node2D):
+		hitablesBody.append(node))
+	hitbox.body_exited.connect(func (node : Node2D):
+		var index := hitablesBody.find(node)
+		if index != -1:
+			hitablesBody.remove_at(index))
+
+## Function to call to do a tackle
+func tackle():
+	if hitbox == null:
+		push_warning("Hitbox not set. doing nothing")
+		return
+		
+	_print("Tackle")
+	
+	## Tackle all targets availables
+	for node in hitablesBody:
+		# If the entity has a stun handler, stun it !
+		var hitHandler = node.get_node("%HitHandler") as StunHandler
+		if (hitHandler != null):
+			hitHandler.stunFor(stunDuration)
+
+
+##Calles by editor to update configuration warning on the script
+func _get_configuration_warnings():
+	var warnings := []
+
+	# === Check hitbox validity
+	if !is_instance_of(hitbox, Area2D) || hitbox == null:
+		warnings.append("An hitbox shall be specified")
+
+	# Returning an empty array means "no warning".
+	return warnings
+
+
+func _print(text : String):
+	if isVerbose:
+		print(text)
