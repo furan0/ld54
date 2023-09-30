f1515010b084e31b7c729b7213d822e373c4e82b
furan0 <augustin.delecroix@gmail.com>
2023-09-30 14:01:55 +0200

    Main menu edited

---
+++ scripts/Player/StunHandler.gd
@@ -0,0 +1,16 @@
+extends Node
+class_name StunHandler
+## StunHandler
+##
+## This script handle player stun and stun exit
+
+## Stun durations dict
+@export var stunDurations : Dictionary
+
+## signal 
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
