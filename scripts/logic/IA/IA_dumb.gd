db97755f7342ec0e16f59e9a360a3c8003f24af1
Antoine Guillemain <antoine.guillemain@gmail.com>
2023-10-02 11:31:19 +0200

    function added for stopiing IA

--- scripts/logic/IA/IA_dumb.gd
+++ scripts/logic/IA/IA_dumb.gd
@@ -7,16 +7,42 @@ signal stop
 signal charge
 
 
-@onready var input_handler := %InputHandler
+@onready var input_handler := %InputHandler as InputHandler
 @export var target_node : Node2D
 @export var start_on_load = true
+## Param de Strategie
+@export var ZAD : Array[Vector2] = []
+@export var frame_react := 8
+##
+
+var papa : Node2D
 ###
-var delatcounter := 0.0
+# Don not modify directly use set_ia_active
+var is_ia_active = false
 var keep_going_global := true
 var timer
 ###
 
+ #Lancer IA
+func start_ia():
+	pass
 
+
+# Autorise l'IA a choisir des action à realiser.
+func set_ia_active(active:bool):
+	var old_state = is_ia_active
+	is_ia_active = active
+	if is_ia_active:
+		if not(old_state):
+			start_ia()
+	else :
+		keep_going_global = false
+		
+## Ne stop pas l'IA
+func stop_action():
+	keep_going_global = false
+
+## Move to a direction an emit a stop.
 func move_to(target_pos : Vector2,threhsold_for_stop=10.0):
 	var keep_going := true
 	var sqrd_thr = threhsold_for_stop * threhsold_for_stop
@@ -59,12 +85,14 @@ func think(tps := 1.0):
 	stop.emit()
 	
 func take_a_break(shouldemit:=true):
-	input_handler.spoofInput("setMove",false)
-	input_handler.spoofInput("move",Vector2.ZERO)
-	if(shouldemit):
-		stop.emit()
+	if(keep_going_global):
+		input_handler.spoofInput("setMove",false)
+		input_handler.spoofInput("move",Vector2.ZERO)
+		if(shouldemit):
+			stop.emit()
 
 func make_charge(target : Node2D, offset:=Vector2.ZERO,tps=3000.0,proxim_threshold:=300000):
+	
 	var keep_going := true
 	input_handler.spoofInput("charge",true)
 	if(target_node!=null):
@@ -99,42 +127,88 @@ func make_guard(target : Node2D):
 	input_handler.spoofInput("move",Vector2.ZERO)
 	stop.emit()
 
-func pick_action():
-	if(target_node != null):
-		keep_going_global=true
-		
-		var p := randf()
-		var offset = (target_node.position + get_parent().position) * 0.5 - target_node.position
-		offset = offset + (offset as Vector2).orthogonal() 
-		if (p<0.05):
-			get_tree().create_timer(0.3).timeout.connect(stop_action)
-			follow(target_node,offset)
-		elif(p<0.3):
-			get_tree().create_timer(0.4).timeout.connect(stop_action)
-			offset = (get_parent().position-target_node.position).orthogonal()  * 2
-			follow(target_node,offset)
-		elif(p<0.4):
-			get_tree().create_timer(0.3).timeout.connect(stop_action)
-			think()
-		elif(p<0.6):
-			get_tree().create_timer(0.5).timeout.connect(stop_action)
-			make_charge(target_node)
-		else:
-			get_tree().create_timer(0.7).timeout.connect(stop_action)
-			make_guard(target_node)
 
-func stop_action():
-	keep_going_global = false
+## Va chercher un pt strategique qui n'est pas sur l'axe joueur / peon
+func go_to_orthogonal_ZAD():
+	var best_pose = ZAD[0]
+	var dir := target_node.position-papa.position
+	var mid_point := (target_node.position+papa.position) * 0.5
+	var best_score = 10000000000.0 # faites pas chier
+	for pos in ZAD:
+		var score = abs((pos-mid_point).dot(dir))
+		if(score < best_score):
+			best_score = score
+			best_pose = pos
+	move_to(best_pose)
+
+## Va chercher un pt strategique qui est le plus loin du joueur et le plus proche du joueur
+func flee():
+	var best_pose = ZAD[0]
+	var dir := target_node.position-papa.position
+	#var mid_point := (target_node.position+papa.position) * 0.5
+	var best_score = 10000000000.0 # faites pas chier
+	for pos in ZAD:
+		var score = (pos-papa.position).dot(dir)
+		if(score < best_score):
+			best_score = score
+			best_pose = pos
+	move_to(best_pose)
+
+## Attaque suivant la position de la target.
+func pick_attaque(threshold_for_charge=40.0):
+	if (target_node.position-papa.position).length_squared() < threshold_for_charge*threshold_for_charge:
+		for i in range(frame_react):
+			# simule un temps de reaction 
+			await get_tree().physics_frame
+		get_tree().create_timer(0.6).timeout.connect(stop_action)
+		input_handler.spoofInput("tackle",null)
+	elif (target_node.position-papa.position).length_squared() < 1.7*1.7*threshold_for_charge*threshold_for_charge:
+		# tkt
+		get_tree().create_timer(0.3).timeout.connect(stop_action)
+		make_guard(target_node)
+	else :
+		for i in range(frame_react):
+			# simule un temps de reaction 
+			await get_tree().physics_frame
+		get_tree().create_timer(0.9).timeout.connect(stop_action)
+		make_charge(target_node)
+
+func pick_action():
+	if(is_ia_active):
+		if(target_node != null):
+			keep_going_global=true
+			
+			var p := randf()
+			var offset = (target_node.position + get_parent().position) * 0.5 - target_node.position
+			offset = offset + (offset as Vector2).orthogonal() 
+			if (p<0.05):
+				get_tree().create_timer(0.3).timeout.connect(stop_action)
+				follow(target_node,offset)
+			elif(p<0.3):
+				get_tree().create_timer(0.4).timeout.connect(stop_action)
+				offset = (get_parent().position-target_node.position).orthogonal()  * 2
+				follow(target_node,offset)
+			elif(p<0.4):
+				get_tree().create_timer(0.3).timeout.connect(stop_action)
+				think()
+			elif(p<0.6):
+				get_tree().create_timer(0.5).timeout.connect(stop_action)
+				make_charge(target_node)
+			else:
+				get_tree().create_timer(0.7).timeout.connect(stop_action)
+				make_guard(target_node)
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	papa = get_parent() as Node2D
 	if(start_on_load):
 		connect("stop",pick_action)
 		pick_action()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	delatcounter += delta
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	delatcounter += delta
 
