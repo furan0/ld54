df1f210590d68895dbc8bdb17f2e5e23fb89163b
furan0 <augustin.delecroix@gmail.com>
2023-10-02 19:49:06 +0200

    tozzjighzeuivbui

--- scripts/audio/audio_effect.gd
+++ scripts/audio/audio_effect.gd
@@ -5,16 +5,6 @@ class_name audio_effect
 
 @export var pitch_distortion := 0.0 
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func play_sound():
 	if(len(audio_clips)<1):
 		return
